1e55fac12716232cb44643cb2bc22996b7eff8b0
brennanijackson <tomjurdyumyum@gmail.com>
2026-09-01 10:33:26 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const portfolioData = {
+    project1: {
+        title: "Project 1 Title",
+        description: "A short description of the project goes here.",
+        images: [
+            "assets/fullsize/project1/img1.jpg",
+            "assets/fullsize/project1/img2.jpg",
+            "assets/fullsize/project1/img3.jpg"
+        ]
+    },
+    // Populate project2 through project7 following this exact schema
+};
+
+let currentProject = '';
+let currentImageIndex = 0;
+
+// Functions for openProject(), closeModal(), and changeImage() will go here.
