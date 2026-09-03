f5f59edf929e9b7c2220436fc2c55ee2d84bb609
brennanijackson <tomjurdyumyum@gmail.com>
2026-09-02 19:57:44 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -9,6 +9,28 @@ const portfolioData = {
             "assets/blue/376_Project3_FINAL-3.jpg",
             "assets/blue/Des376ProjectThree_2018-1.jpg"
         ]
+    },
+    project2: {
+        title: "Cornhole boards",
+        description: "A short description of project 2 goes here.",
+        images: [
+            "assets/cornhole/IMG_8823.jpg",
+            "assets/cornhole/IMG_8850.jpg",
+            "assets/cornhole/IMG_9034.jpg",
+            "assets/cornhole/IMG_9039.jpg",
+            "assets/cornhole/IMG_9102.jpg",
+            "assets/cornhole/IMG_9104.jpg"
+        ]
+    },
+    project3: {
+        title: "Monorail branding",
+        description: "A short description of project 3 goes here.",
+        images: [
+            "assets/monorail/Jackson_Brand1.jpg",
+            "assets/monorail/Jackson_Brand2.jpg",
+            "assets/monorail/Jackson_Brand3.jpg",
+            "assets/monorail/Jackson_BrandHeader.jpg"
+        ]
     }
     // Add project2 through project7 here following this exact structure
 };
